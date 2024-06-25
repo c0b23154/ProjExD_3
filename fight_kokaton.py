9533fdbb349f81c04936db79920d7903acfaaecb
C0B23154 <c0b23154c4@edu.teu.ac.jp>
2024-06-25 16:42:21 +0900

    追加機能3　explosion

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -141,13 +141,24 @@ class Bomb:
 
 
 class Score():
+    """
+    スコアに関するクラス
+    """
     def __init__(self):
+        """
+        フォントと文字色を設定し、文字列Surfaceを生成
+        """
         self.fonto = pg.font.SysFont("hgp創英角ﾎﾟｯﾌﾟ体", 30)
         self.score = 0
         self.img = self.fonto.render(f"スコア: {self.score}", 0, (0, 0, 255))
         self.rct = (100, HEIGHT-50)
 
-    def update(self, screen):
+    def update(self, screen: pg.Surface):
+        """
+        現在のスコアを表示させる文字列Surfaceの生成
+        スクリーンにblit
+        引数 screen：画面Surface
+        """
         self.img = self.fonto.render(f"スコア: {self.score}", 0, (0, 0, 255))
         screen.blit(self.img, self.rct)
 
@@ -159,9 +170,10 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bird = Bird((300, 200))
     beam = None
+    be = []
     # bomb = Bomb((255, 0, 0), 10)
     bombs = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS)]
-    score = Score()
+    score = Score()  # Scoreインスタンスの生成
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -170,8 +182,11 @@ def main():
                 return
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 # スペースキー押下でBeamクラスのインスタンス生成
-                beam = Beam(bird)            
-        screen.blit(bg_img, [0, 0])
+                beam = Beam(bird)  
+                be.append(beam)
+                for i in be:
+
+            screen.blit(bg_img, [0, 0])
         
         for bomb in bombs:
             if bird.rct.colliderect(bomb.rct):
@@ -191,11 +206,11 @@ def main():
                 if bombs[i].rct.colliderect(beam.rct):
                     bombs[i] = None
                     beam = None
-                    score.score += 1
+                    score.score += 1  # 爆弾を撃ち落としたらスコアアップ
                     bird.change_img(6, screen)
         bombs = [bomb for bomb in bombs if bomb is not None]
 
-        score.update(screen)
+        score.update(screen)  # updateメゾットを呼び出してスコアを描画
         pg.display.flip()
 
         key_lst = pg.key.get_pressed()
